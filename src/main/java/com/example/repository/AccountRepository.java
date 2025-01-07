58498ab025154d628e8862f1fd327fbc1eacc8d8
Mark Bahr <64796979+MarkBahr@users.noreply.github.com>
2025-01-07 12:07:07 -0600

    Comments updated in AccountRepository.java

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -4,11 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.entity.Account;
 
+/**
+ * A Repository interface that retrieves data from the database, using Spring Data JPA
+ */
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer>{
 
+	// Method to retrieve an account by the specified username and password.
 	public Account findByUsernameAndPassword(String username, String password);
-	
+
+	// Method to find a user by username
 	public Account findByUsername(String username);
 
 }
