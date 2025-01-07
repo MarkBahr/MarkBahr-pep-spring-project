6cc035b9e0ac645397334cc8a1f9e78f1fd94519
Mark Bahr <64796979+MarkBahr@users.noreply.github.com>
2025-01-07 12:29:47 -0600

    Comments updated for MessageService.java

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,16 +1,16 @@
 package com.example.service;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+// Imports
 import org.springframework.stereotype.Service;
-
 import com.example.repository.AccountRepository;
 import com.example.repository.MessageRepository;
 import com.example.entity.Message;
-
-// import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class that services the Message entity
+ */
 @Service
 public class MessageService {
 
@@ -23,46 +23,66 @@ public class MessageService {
         this.accountRepository = accountRepository;
     }
 
-    // Check if User exists
+    /**
+     * Method that checks if a user exists by accountId
+     * @param accountId from Account entity
+     * returns true if user exists, false otherwise
+     */
     public boolean userExists(Integer accountId) {
     	boolean isPresent = false;
     	isPresent = accountRepository.existsById(accountId);
     	return isPresent;
     }
 
-    // Insert a record
+    
     /**
-     * User Story 3 - INSERT a message record in the database for message creation, return the message created
+     * Method for User Story 3 - INSERT a message record in the database for message creation.
      * @param message
+     * @return the message created
      */ 
 	public Message insertMessage(Message message) {
 	    return messageRepository.save(message);
 	}
+
 	
-	// JPA/HIbernate -> database to make static list
-	//public List<Message> findAll()
     /**
-     * User Story 4 - Retrieves all messages from the database
+     * Method for User Story 4 - Retrieves all messages from the database
      * @return a list of all messages
      */
     public List<Message> getAllMessages() { 
         return messageRepository.findAll(); 
     }
 
-    // Get message by id
+    /**
+     * Method that retrieves message by messageId
+     * @param messageId from Message entity
+     * @return specified message if it exists, if not returns empty optional
+     */
     public Message getMessageById(Integer messageId) {
         Optional<Message> optionalMessage = messageRepository.findById(messageId);
         Message message = optionalMessage.orElse(null);
         return message;
     }
 
+
+    /**
+     * Method that deletes a message by messageId
+     * @param messageId from Message entity
+     * @return the number of deleted messages
+     */
     public Integer deleteMessage(Integer messageId) {
     	messageRepository.deleteById(messageId);
     	int numDeleted = (int) messageRepository.count();
         Integer gonner = numDeleted;
         return gonner;
     }
-    
+
+	
+    /**
+     * Method that checks if a message exists by messageId
+     * @param messageId from Message entity
+     * @return true if the message exists, false if it does not exist
+     */
     public boolean messageExists(Integer messageId) {
     	boolean isPresent = false;
     	isPresent = messageRepository.existsById(messageId);
@@ -70,9 +90,10 @@ public class MessageService {
     }
 
     /**
-     * User Story 7: Update message
+     * Method for User Story 7: Update message
      * @param id - id of message to be updated
      * @param repplacement - updated text for messageText
+     * @return the persisted (updated) message
      */
     public Message updateMessage(Integer id, String replacement){
         
@@ -85,7 +106,7 @@ public class MessageService {
     }
 
     /**
-     * User Story 8: - Find all messages by a particular user
+     * Method for User Story 8: - Find all messages by a particular user
      * 
      * @param postedBy - accountId for messages
      * @return list of messages posted by that accountId
