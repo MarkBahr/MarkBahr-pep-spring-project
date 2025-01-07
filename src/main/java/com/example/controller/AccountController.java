ed36105e4d903f5eaeec54991bf8a13833ea1cef
Mark Bahr <64796979+MarkBahr@users.noreply.github.com>
2025-01-07 11:44:15 -0600

    Comments added to AccountController.java

--- src/main/java/com/example/controller/AccountController.java
+++ src/main/java/com/example/controller/AccountController.java
@@ -1,33 +1,47 @@
 package com.example.controller;
 
-import java.util.List;
+/**
+*  This class is a controller that handles requests for accounts.
+*/
 
+// Imports
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.entity.Account;
 import com.example.service.AccountService;
+
 
 @RestController
 public class AccountController {
 	
 	private AccountService service;
 	
+	// Constructor initializes instance of AccountService class
 	public AccountController(AccountService service) {
 		this.service = service;
 	}
+
 	
-	
+	/**
+	 * HANDLER for retrieving all accounts
+	 * @return a list of all accounts retrieved from the database
+	 */
 	@GetMapping("accounts")
 	public List<Account> retrieveAllAccounts() {
 		return service.getAllAccounts();
 	}
+
 	
+	/**
+	 * HANDLER method for creating an account upon user registration
+	 * @param account
+	 * @return
+	 */
 	@PostMapping("register")
 	public ResponseEntity<Account> createAccount(@RequestBody Account account) {
 		
@@ -47,7 +61,12 @@ public class AccountController {
 			return ResponseEntity.status(HttpStatus.OK).body(newAccount); // Return the account with 200 status
 		}
 	}
-	
+
+
+	/**
+      	 * HANDLER method for user login. If username & password correct, the response body should 
+      	 * contain a JSON of the account in the response body. If not, the response status is unathorized.
+      	 */
 	@PostMapping("login")
 	public ResponseEntity<Account> accountLogin(@RequestBody Account account) {
 		Account confirmAccount =  service.getAccount(account.getUsername(), account.getPassword());
